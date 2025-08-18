e198340468f3d48242808e09220454244ad36dd4
hajong <gkwhdtn95051@gmail.com>
2025-08-18 15:25:29 +0900

    로그인 요청 모델 추가 및 로그인 핸들러와 인증 미들웨어 통합: 컨테이너 구조 수정, 라우터에 인증 기능 추가

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,16 +2,23 @@ package router
 
 import (
 	"julia/internal/di"
+	"julia/internal/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter(router *gin.Engine, c *di.Container) {
 	user := router.Group("/users")
+	user.Use(middlewares.AuthMiddleware(c.AuthService))
 	{
 		user.GET("/:id", c.UserHandler.GetByID)
 		user.POST("/", c.UserHandler.Create)
 		user.PUT("/:id", c.UserHandler.Update)
 		user.DELETE("/:id", c.UserHandler.Delete)
+	}
+	auth := router.Group("/auth")
+	{
+		auth.POST("/login", c.LoginHandler.Login)
+		auth.POST("/logout", middlewares.AuthMiddleware(c.AuthService), c.LoginHandler.Logout)
 	}
 }
